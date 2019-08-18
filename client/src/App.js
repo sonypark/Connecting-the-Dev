4d76304a615aeba9b15addf88d2f26d55b530a03
Sony <sonypark0204@gmail.com>
2019-08-18 22:12:49 +0900

    [feat] Alert action
    - Alert action 구현
    - 3초 뒤 자동으로 사라지도록 구현

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 // import Redux
 import {Provider} from 'react-redux';
 import store from './store';
@@ -18,6 +19,7 @@ const App = () => (
                 <Navbar/>
                 <Route exact path='/' component={Landing}/>
                 <section className="container">
+                    <Alert/>
                     <Switch>
                         <Route exact path="/register" component={Register}/>
                         <Route exact path="/login" component={Login}/>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, {Fragment, useState} from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {setAlert} from '../../actions/alert'
+import PropTypes from 'prop-types'
 
-const Register = () => {
+const Register = ({setAlert}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -18,7 +21,7 @@ const Register = () => {
     const onSubmit = e => {
         e.preventDefault();
         if (password !== password2){
-            console.log('Passwords does not match');
+            setAlert('Passwords don\'t match', 'danger');
         }else {
             console.log(formData);
         }
@@ -81,4 +84,9 @@ const Register = () => {
         </Fragment>
     )
 };
-export default Register;
+
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, {setAlert})(Register);
