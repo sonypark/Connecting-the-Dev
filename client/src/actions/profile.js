6488972d2a5f3fb49e9b74e77cb42be048f369e0
Sony <sonypark0204@gmail.com>
2019-08-21 16:05:20 +0900

    [feat] Create profile action
    - 프로필 action 구현
    - 프로필 정보를 입력하고 submit 버튼을 누르면 DB에 저장되는 것 까지 구현

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -16,7 +16,43 @@ export const getCurrentUserProfile = () => async dispatch => {
     } catch (err) {
         dispatch({
             type: PROFILE_ERROR,
-            payload: {msg: err.response.data.msg, status: err.response.status}
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+};
+
+// 프로필 작성 및 수정
+export const createProfile = (formData, history, edit=false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.post('/api/profile', formData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+        if(!edit){
+            history.push('/dashboard');
+        }
+
+    }catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
         })
     }
 };
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,9 +1,10 @@
 import React, {Fragment, useState} from 'react';
+import {Link, withRouter } from "react-router-dom";
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import { createProfile} from "../../actions/profile";
 
-
-const CreateProfile = props => {
+const CreateProfile = ({createProfile, history}) => {
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -39,6 +40,11 @@ const CreateProfile = props => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
 
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history);
+    };
+
     return (
         <Fragment>
             <h1 className="large text-primary">
@@ -49,7 +55,7 @@ const CreateProfile = props => {
                 profile stand out
             </p>
             <small>* = required field</small>
-            <form className="form">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
                     <select name="status" value={status} onChange={e => onChange(e)}>
                         <option value="0">* Select Professional Status</option>
@@ -156,6 +162,8 @@ const CreateProfile = props => {
     );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+    createProfile: PropTypes.func.isRequired,
+};
 
-export default CreateProfile;
+export default connect(null, {createProfile})(withRouter(CreateProfile));
