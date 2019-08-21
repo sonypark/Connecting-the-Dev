64f7baa3cf18eaa4e12f6f1187a1e3b972d0465c
Sony <sonypark0204@gmail.com>
2019-08-21 20:51:35 +0900

    [feat] Add experience in profile
    - 프로필에 experience 추가 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
 import PrivateRoute from './components/route/PrivateRoute';
 import {loadUser} from './actions/auth';
 import setAuthToken from "./utils/setAuthToken";
@@ -43,6 +44,7 @@ const App = () => {
                             <PrivateRoute exact path="/dashboard" component={Dashboard}/>
                             <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
                             <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
+                            <PrivateRoute exact path="/add-experience" component={AddExperience}/>
                         </Switch>
                     </section>
                 </Fragment>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {setAlert} from "./alert";
 
-import {GET_PROFILE, PROFILE_ERROR} from "./types";
+import {GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE} from "./types";
 
 // GET 현재 유저 프로필
 export const getCurrentUserProfile = () => async dispatch => {
@@ -42,6 +42,73 @@ export const createProfile = (formData, history, edit=false) => async dispatch =
         if(!edit){
             history.push('/dashboard');
         }
+
+    }catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+};
+
+
+// Add experience
+export const addExperience = (formData, history) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.put('/api/profile/experience', formData, config);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Experience Added', 'success'));
+        history.push('/dashboard');
+
+    }catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+};
+
+// Add education
+export const addEducation = (formData, history) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.put('/api/profile/education', formData, config);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Education Added', 'success'));
+        history.push('/dashboard');
 
     }catch (err) {
         const errors = err.response.data.errors;
