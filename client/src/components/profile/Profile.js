b7795b62f42fe4b61842220adb68350c5801f965
Sony <sonypark0204@gmail.com>
2019-08-22 22:46:06 +0900

    [feat] Create bio, skills box on profile
    - 프로필 창에서 자기소개와 기술 스택 박스 구현

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import {connect} from 'react-redux';
 import {Link} from "react-router-dom";
 import Spinner from '../layout/Spinner'
 import {getProfileById} from "../../actions/profile";
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({getProfileById, profile: {profile, loading}, auth, match}) => {
     useEffect(() => {
@@ -23,6 +25,10 @@ const Profile = ({getProfileById, profile: {profile, loading}, auth, match}) => 
                             Edit Profile
                         </Link>
                     )}
+                    <div className='profile-grid my-1'>
+                        <ProfileTop profile={profile} />
+                        <ProfileAbout profile={profile} />
+                    </div>
                 </Fragment>
             )}
         </Fragment>
