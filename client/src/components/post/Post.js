80e85581ca56c3da9bf2ecd267e9baab6fc0590e
Sony <sonypark0204@gmail.com>
2019-08-23 22:01:08 +0900

    [feat] Delete comment on posts
    - 게시물 댓글 삭제

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import {getPost} from "../../actions/post";
 import PostItem from '../posts/PostItem';
 import {Link} from "react-router-dom";
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem'
 
 const Post = ({getPost, post: {post, loading}, match }) => {
     useEffect(()=>{
@@ -18,6 +19,11 @@ const Post = ({getPost, post: {post, loading}, match }) => {
             </Link>
             <PostItem post={post} showActions={false}/>
             <CommentForm postId={post._id}/>
+            <div className="comments">
+                {post.comments.map(comment => (
+                    <CommentItem key={comment._id} comment={comment} postId={post._id}/>
+                ))}
+            </div>
         </Fragment>
     );
 };
