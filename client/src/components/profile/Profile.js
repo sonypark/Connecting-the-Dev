ad5d15934ecc3761786d32dedb2d67e2bb6db959
Sony <sonypark0204@gmail.com>
2019-08-23 15:22:39 +0900

    [feat] Create experience, education info on each profile
    - 각 유저의 프로필 열람 페이지에 experience, education 정보 열람 가능하도록 구현

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import Spinner from '../layout/Spinner'
 import {getProfileById} from "../../actions/profile";
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 
 const Profile = ({getProfileById, profile: {profile, loading}, auth, match}) => {
     useEffect(() => {
@@ -28,6 +30,24 @@ const Profile = ({getProfileById, profile: {profile, loading}, auth, match}) => 
                     <div className='profile-grid my-1'>
                         <ProfileTop profile={profile} />
                         <ProfileAbout profile={profile} />
+
+                        <div className="profile-exp bg-white p-2">
+                            <h2 className="text-primary">Experience</h2>
+                            {profile.experience.length > 0 ? (<Fragment>
+                                {profile.experience.map(experience => (
+                                    <ProfileExperience key={experience.id} experience={experience}/>
+                                ))}
+                            </Fragment>) : (<h4>No experience credentials</h4>)}
+                        </div>
+
+                        <div className="profile-edu bg-white p-2">
+                            <h2 className="text-primary">Education</h2>
+                            {profile.education.length > 0 ? (<Fragment>
+                                {profile.education.map(education => (
+                                    <ProfileEducation key={education.id} education={education}/>
+                                ))}
+                            </Fragment>) : (<h4>No education credentials</h4>)}
+                        </div>
                     </div>
                 </Fragment>
             )}
